df8838799603af251b3486cd7e7a3fe28fb68ace
nicolasguasca1 <nicolas.guasca@gmail.com>
2021-11-23 23:57:40 +0100

    First iteration

--- src/pages/Presentation.js
+++ src/pages/Presentation.js
@@ -1,12 +1,46 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBook, faExternalLinkAlt, faTimesCircle, faCheckCircle, faCalendarAlt, faCodeBranch, faShoppingCart, faFolder, faMapMarkedAlt, faPager, faFileCode, faDownload } from "@fortawesome/free-solid-svg-icons";
-import { faBootstrap, faGithub, faJs, faReact, faSass } from "@fortawesome/free-brands-svg-icons";
-import { Col, Row, Card, Image, Button, Container, ListGroup, Tooltip, OverlayTrigger, Form, Navbar, Nav, Badge } from '@themesberg/react-bootstrap';
-import { Link } from 'react-router-dom';
-import { HashLink } from 'react-router-hash-link';
+import {
+  faBook,
+  faExternalLinkAlt,
+  faTimesCircle,
+  faCheckCircle,
+  faCalendarAlt,
+  faCodeBranch,
+  faShoppingCart,
+  faFolder,
+  faMapMarkedAlt,
+  faPager,
+  faFileCode,
+  faDownload
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faBootstrap,
+  faGithub,
+  faJs,
+  faReact,
+  faSass
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  Col,
+  Carousel,
+  Row,
+  Card,
+  Image,
+  Button,
+  Container,
+  ListGroup,
+  Tooltip,
+  OverlayTrigger,
+  Form,
+  Navbar,
+  Nav,
+  Badge
+} from "@themesberg/react-bootstrap";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 import Code from "../components/CodeEditor";
-import GitHubButton from 'react-github-btn';
+import GitHubButton from "react-github-btn";
 
 import { Routes } from "../routes";
 import ThemesbergLogoIcon from "../assets/img/themesberg.svg";
@@ -19,6 +53,9 @@ import ReactMockupImg from "../assets/img/react-mockup.png";
 import BS5IllustrationsImg from "../assets/img/illustrations/bs5-illustrations.svg";
 import BS5Logo from "../assets/img/technologies/bootstrap-5-logo.svg";
 import ReactLogo from "../assets/img/technologies/react-logo.svg";
+import mapas from "../assets/img/mapas.png";
+import lupa from "../assets/img/lupa.png";
+import truck_logo from "../assets/img/truck_logo.png";
 
 import pages from "../data/pages";
 import features from "../data/features";
@@ -29,12 +66,21 @@ export default () => {
 
     return (
       <Col xs={6} className="mb-5">
-        <Card.Link as={Link} to={link} className="page-preview page-preview-lg scale-up-hover-2">
-          <Image src={image} className="shadow-lg rounded scale" alt="Dashboard page preview" />
+        <Card.Link
+          as={Link}
+          to={link}
+          className="page-preview page-preview-lg scale-up-hover-2"
+        >
+          <Image
+            src={image}
+            className="shadow-lg rounded scale"
+            alt="Dashboard page preview"
+          />
 
           <div className="text-center show-on-hover">
             <h6 className="m-0 text-center text-white">
-              {name} <FontAwesomeIcon icon={faExternalLinkAlt} className="ms-2" />
+              {name}{" "}
+              <FontAwesomeIcon icon={faExternalLinkAlt} className="ms-2" />
             </h6>
           </div>
         </Card.Link>
@@ -66,12 +112,20 @@ export default () => {
 
     return (
       <OverlayTrigger
-        trigger={['hover', 'focus']}
+        trigger={["hover", "focus"]}
         placement="left"
         overlay={<Tooltip>{tooltip}</Tooltip>}
       >
-        <li data-toggle="tooltip" data-placement="left" title="Main folder that you will be working with">
-          <FontAwesomeIcon icon={icon ? icon : faFolder} className={`${color} me-2`} /> {name}
+        <li
+          data-toggle="tooltip"
+          data-placement="left"
+          title="Main folder that you will be working with"
+        >
+          <FontAwesomeIcon
+            icon={icon ? icon : faFolder}
+            className={`${color} me-2`}
+          />{" "}
+          {name}
         </li>
       </OverlayTrigger>
     );
@@ -79,58 +133,161 @@ export default () => {
 
   return (
     <>
-      <Navbar variant="dark" expand="lg" bg="dark" className="navbar-transparent navbar-theme-primary sticky-top">
+      <Navbar
+        variant="dark"
+        expand="lg"
+        bg="dark"
+        className="navbar-transparent navbar-theme-primary sticky-top bg-gradient"
+      >
         <Container className="position-relative justify-content-between px-3">
-          <Navbar.Brand as={HashLink} to="#home" className="me-lg-3 d-flex align-items-center">
-            <Image src={ReactHero} />
-            <span className="ms-2 brand-text d-none d-md-inline">Volt React</span>
+          <Navbar.Brand
+            as={Link}
+            to="/"
+            className="me-lg-3 d-flex align-items-center"
+          >
+            <Image src={truck_logo} />
+            <span className="ms-2 brand-text d-none d-md-inline">Tripool</span>
           </Navbar.Brand>
 
           <div className="d-flex align-items-center">
             <Navbar.Collapse id="navbar-default-primary">
               <Nav className="navbar-nav-hover align-items-lg-center">
-                <Nav.Link as={HashLink} to="#features">Features</Nav.Link>
-                <Nav.Link as={HashLink} to="#pages">Pages</Nav.Link>
-                <Nav.Link as={HashLink} to="#folder" className="d-sm-none d-xl-inline">Folder Structure</Nav.Link>
-                <Nav.Link as={HashLink} to="#getting-started">Getting Started</Nav.Link>
-                <Nav.Link as={HashLink} to="#download">Upgrade to Pro</Nav.Link>
+                <Nav.Link as={HashLink} to="dashboard/overview">
+                  Explorar Dashboard
+                </Nav.Link>
+                {/* <Nav.Link as={HashLink} to="#pages">
+                  Pages
+                </Nav.Link>
+                <Nav.Link
+                  as={HashLink}
+                  to="#folder"
+                  className="d-sm-none d-xl-inline"
+                >
+                  Folder Structure
+                </Nav.Link>
+                <Nav.Link as={HashLink} to="#getting-started">
+                  Getting Started
+                </Nav.Link> */}
+                <Nav.Link as={HashLink} to="#download">
+                  Inicio de sesión
+                </Nav.Link>
               </Nav>
             </Navbar.Collapse>
-            <Button as={HashLink} to="#download" variant="outline-white" className="ms-3"><FontAwesomeIcon icon={faDownload} className="me-1" /> Download</Button>
+            <Button
+              as={HashLink}
+              to="#download"
+              variant="outline-white"
+              className="ms-3"
+            >
+              Registro
+            </Button>
           </div>
         </Container>
       </Navbar>
-      <section className="section-header overflow-hidden pt-5 pt-lg-6 pb-9 pb-lg-12 bg-primary text-white" id="home">
+      <section
+        className="section-header overflow-hidden pt-5 pt-lg-6 pb-9 pb-lg-12 bg-primary text-white bg-gradient"
+        id="home"
+      >
         <Container>
           <Row>
             <Col xs={12} className="text-center">
-              <div className="react-big-icon d-none d-lg-block"><span className="fab fa-react"></span></div>
+              {/* <div className="react-big-icon d-none d-lg-block">
+                <span className="fab fa-react"></span>
+              </div>
               <h1 className="fw-bolder text-secondary">Volt React Dashboard</h1>
-              <p className="text-muted fw-light mb-5 h5">Open source powered by React.js and Bootstrap 5</p>
+              <p className="text-muted fw-light mb-5 h5">
+                Open source powered by React.js and Bootstrap 5
+              </p>
               <div className="d-flex align-items-center justify-content-center">
-                <Button variant="secondary" as={Link} to={Routes.DashboardOverview.path} className="text-dark me-3">
-                  Explore dashboard <FontAwesomeIcon icon={faExternalLinkAlt} className="d-none d-sm-inline ms-1" />
+                <Button
+                  variant="secondary"
+                  as={Link}
+                  to={Routes.DashboardOverview.path}
+                  className="text-dark me-3"
+                >
+                  Explore dashboard{" "}
+                  <FontAwesomeIcon
+                    icon={faExternalLinkAlt}
+                    className="d-none d-sm-inline ms-1"
+                  />
                 </Button>
-                <GitHubButton className="mt-lg-2" href="https://github.com/themesberg/volt-react-dashboard" data-size="large" data-show-count="true" aria-label="Star themesberg/volt-react-dashboard on GitHub">Star</GitHubButton>
+                <GitHubButton
+                  className="mt-lg-2"
+                  href="https://github.com/themesberg/volt-react-dashboard"
+                  data-size="large"
+                  data-show-count="true"
+                  aria-label="Star themesberg/volt-react-dashboard on GitHub"
+                >
+                  Star
+                </GitHubButton>
               </div>
               <div className="d-flex justify-content-center flex-column mb-6 mb-lg-5 mt-5">
                 <div className="text-center">
                   <a href="https://themesberg.com" target="_blank">
-                    <Image src={ThemesbergLogoIcon} height={25} width={25} className="mb-3" alt="Themesberg Logo" />
-                    <p className="text-muted font-small m-0">A Themesberg production</p>
+                    <Image
+                      src={ThemesbergLogoIcon}
+                      height={25}
+                      width={25}
+                      className="mb-3"
+                      alt="Themesberg Logo"
+                    />
+                    <p className="text-muted font-small m-0">
+                      A Themesberg production
+                    </p>
                   </a>
+                </div>
+              </div> */}
+
+              <div className="header">
+                <div className="p-5 ">
+                  <Carousel className="container p-5">
+                    <Carousel.Item interval={3000}>
+                      <img
+                        className="d-block"
+                        src={mapas}
+                        alt="mapas"
+                        width="25%"
+                      />
+                      <Carousel.Caption className="text-end">
+                        <h3>A dónde quieras llegar</h3>
+                        <p>
+                          Utiliza nuestras rutas para llevar tus productos en el
+                          menor tiempo posible.
+                        </p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={3000}>
+                      <img
+                        className="d-block"
+                        src={lupa}
+                        alt="lupa"
+                        width="25%"
+                      />
+                      <Carousel.Caption className="text-end">
+                        <h3>Manten el control de tus datos</h3>
+                        <p>
+                          Te damos las herramientas para que puedas ver la
+                          evolución de tus órdenes en tiempo real
+                        </p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                  </Carousel>
                 </div>
               </div>
             </Col>
           </Row>
           <figure className="position-absolute bottom-0 left-0 w-100 d-none d-md-block mb-n2">
-            <svg className="fill-soft" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3000 185.4">
+            <svg
+              className="fill-soft"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 3000 185.4"
+            >
               <path d="M3000,0v185.4H0V0c496.4,115.6,996.4,173.4,1500,173.4S2503.6,115.6,3000,0z" />
             </svg>
           </figure>
         </Container>
       </section>
-      <div className="section pt-0">
+      {/* <div className="section pt-0">
         <Container className="mt-n10 mt-lg-n12 z-2">
           <Row className="justify-content-center">
             <Col xs={12}>
@@ -478,61 +635,140 @@ export default () => {
             </Col>
           </Row>
         </Container>
-      </section>
-      <footer className="footer py-6 bg-dark text-white">
+      </section> */}
+      <footer className="footer py-6 bg-dark text-white bg-gradient">
         <Container>
           <Row>
             <Col md={4}>
-              <Navbar.Brand as={HashLink} to="#home" className="me-lg-3 mb-3 d-flex align-items-center">
-                <Image src={ReactHero} />
-                <span className="ms-2 brand-text">Volt React</span>
+              <Navbar.Brand
+                as={HashLink}
+                to="#home"
+                className="me-lg-3 mb-3 d-flex align-items-center"
+              >
+                <Image src={truck_logo} />
+                <span className="ms-2 brand-text">Tripool</span>
               </Navbar.Brand>
-              <p>Volt React is a free and open source admin dashboard template powered by React.js and Bootstrap 5.</p>
+              <p>
+                Nuestro objetivo es brindar el mejor servicio de logística para
+                sus necesidades de transporte.
+              </p>
             </Col>
             <Col xs={6} md={2} className="mb-5 mb-lg-0">
-              <span className="h5">Themesberg</span>
+              <span className="h5">Información</span>
               <ul className="links-vertical mt-2">
-                <li><Card.Link target="_blank" href="https://themesberg.com/blog">Blog</Card.Link></li>
-                <li><Card.Link target="_blank" href="https://themesberg.com/products">Products</Card.Link></li>
-                <li><Card.Link target="_blank" href="https://themesberg.com/about">About Us</Card.Link></li>
-                <li><Card.Link target="_blank" href="https://themesberg.com/contact">Contact Us</Card.Link></li>
+                <li>
+                  <Card.Link target="_blank" href="/">
+                    Tarifas
+                  </Card.Link>
+                </li>
+                <li>
+                  <Card.Link target="_blank" href="/">
+                    Cobertura geográfica
+                  </Card.Link>
+                </li>
+                <li>
+                  <Card.Link
+                    target="_blank"
+                    href="https://themesberg.com/about"
+                  >
+                    Horarios de atención
+                  </Card.Link>
+                </li>
+                <li>
+                  <Card.Link
+                    target="_blank"
+                    href="https://themesberg.com/contact"
+                  >
+                    Contáctenos
+                  </Card.Link>
+                </li>
               </ul>
             </Col>
             <Col xs={6} md={2} className="mb-5 mb-lg-0">
-              <span className="h5">Other</span>
+              <span className="h5">Enlaces de interés</span>
               <ul className="links-vertical mt-2">
                 <li>
-                  <Card.Link as={Link} to={Routes.DocsQuickStart.path} target="_blank">Getting started</Card.Link>
+                  <Card.Link
+                    as={Link}
+                    to={Routes.DocsQuickStart.path}
+                    target="_blank"
+                  >
+                    Preguntas frecuentes
+                  </Card.Link>
                 </li>
-                <li><Card.Link as={Link} to={Routes.DocsChangelog.path} target="_blank">Changelog</Card.Link></li>
-                <li><Card.Link target="_blank" href="https://themesberg.com/licensing">License</Card.Link></li>
+                <li>
+                  <Card.Link
+                    as={Link}
+                    to={Routes.DocsChangelog.path}
+                    target="_blank"
+                  >
+                    Sobre nosotros
+                  </Card.Link>
+                </li>
+                <li>
+                  <Card.Link
+                    target="_blank"
+                    href="https://themesberg.com/licensing"
+                  >
+                    Términos y condiciones de servicio
+                  </Card.Link>
+                </li>
               </ul>
             </Col>
             <Col xs={12} md={4} className="mb-5 mb-lg-0">
-              <span className="h5 mb-3 d-block">Subscribe</span>
+              {/* <span className="h5 mb-3 d-block">Subscribe</span>
               <form action="#">
                 <div className="form-row mb-2">
                   <div className="col-12">
-                    <input type="email" className="form-control mb-2" placeholder="example@company.com" name="email" aria-label="Subscribe form" required />
+                    <input
+                      type="email"
+                      className="form-control mb-2"
+                      placeholder="example@company.com"
+                      name="email"
+                      aria-label="Subscribe form"
+                      required
+                    />
                   </div>
                   <div className="col-12">
-                    <button type="submit" className="btn btn-secondary text-dark shadow-soft btn-block" data-loading-text="Sending">
+                    <button
+                      type="submit"
+                      className="btn btn-secondary text-dark shadow-soft btn-block"
+                      data-loading-text="Sending"
+                    >
                       <span>Subscribe</span>
                     </button>
                   </div>
                 </div>
-              </form>
-              <p className="text-muted font-small m-0">We’ll never share your details. See our <Card.Link className="text-white" href="#">Privacy Policy</Card.Link></p>
+              </form> */}
             </Col>
           </Row>
           <hr className="bg-gray my-5" />
           <Row>
-            <Col className="mb-md-2">
-              <Card.Link href="https://themesberg.com" target="_blank" className="d-flex justify-content-center">
-                <Image src={ThemesbergLogo} height={35} className="d-block mx-auto mb-3" alt="Themesberg Logo" />
-              </Card.Link>
-              <div className="d-flex text-center justify-content-center align-items-center" role="contentinfo">
-                <p className="font-weight-normal font-small mb-0">Copyright © Themesberg 2019-<span className="current-year">2021</span>. All rights reserved.</p>
+            <Col className="d-flex justify-content-center justify-content-lg-between p-4">
+              {/* <Card.Link
+                href="https://themesberg.com"
+                target="_blank"
+                className="d-flex justify-content-center"
+              >
+                <Image
+                  src={ThemesbergLogo}
+                  height={35}
+                  className="d-block mx-auto mb-3"
+                  alt="Themesberg Logo"
+                />
+              </Card.Link> */}
+              <div className="text-left" role="contentinfo">
+                <p className="font-weight-normal font-small mb-0 ">
+                  Copyright © Tripool SAS 2021. Todos los derechos reservados.
+                </p>
+              </div>
+              <div className="text-right" role="contentinfo">
+                <p className="text-muted font-small m-0 ">
+                  Consulte nuestra{" "}
+                  <Card.Link className="text-white" href="#">
+                    Política de Privacidad
+                  </Card.Link>
+                </p>
               </div>
             </Col>
           </Row>
