3f08a566fc629721aa4846c7e34acbe940b60988
nicolasguasca1 <nicolas.guasca@gmail.com>
2021-11-23 22:03:10 +0100

    Users page created

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@
 
 // * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. Please contact us to request a removal.
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter, BrowserRouter } from "react-router-dom";
 
 // core styles
 import "./scss/volt.scss";
@@ -28,9 +28,9 @@ import HomePage from "./pages/HomePage";
 import ScrollToTop from "./components/ScrollToTop";
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <ScrollToTop />
     <HomePage />
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
